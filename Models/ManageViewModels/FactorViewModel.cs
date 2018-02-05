32653b81b355e19864ee113cc03098934d95d9f5
unknown <almir@face2face.inf.br>
2018-02-05 16:12:18 -0200

    teste primeiro commit

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teste3.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
